e77c8d57cf28280b3534eadddba6b1f81e42a8b2
Robert Gilmour <2022607g@student.gla.ac.uk>
2017-01-25 21:44:02 +0000

    Finished chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -3,4 +3,17 @@ from rango.models import Category, Page
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None, {'fields': ['category', 'title', 'url']}),
+    ('Page Statistics', {'fields': ['views']}),
+    ]
+
+    list_display = ('title', 'category', 'url')
+
+    list_filter = ['category']
+
+    search_fields = ['title']
+
+admin.site.register(Page, PageAdmin)
