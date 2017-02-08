8ba09b73e1c7adc20271a0e17f0dcf412a20dd2f
Robert Gilmour <2022607g@student.gla.ac.uk>
2017-02-08 22:48:33 +0000

    Finished 9.6 but can't upload images

--- rango/models.py
+++ rango/models.py
@@ -31,10 +31,10 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
-class UserProfiles(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User)
     website = models.URLField(blank=True)
-    picture = models.ImageField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
         return self.user.username
